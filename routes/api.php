ce6c8502aab770b0f2c3dd2d13890defc048d62c
Nellafitriani09 <114406491+Nellafitrianiii@users.noreply.github.com>
2023-12-01 22:54:36 +0700

    lanjutan misi 4

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\v1\DesaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/desa',[DesaController::class,'getData']);
+Route::get('/desa/{id}', [DesaController::class, 'getDetail']);
+Route::post('/desa', [DesaController::class, 'addDesa']);
+Route::put('/desa/{id}', [DesaController::class, 'updateDesa']);
+Route::delete('/desa/{id}', [DesaController::class, 'deleteDesa']);
